7d129b699de4662d7c489c77d8cbc28e81beaea2
Ola Okegbemi <okegbemiolatunde@gmail.com>
2024-04-20 00:27:49 +0100

    config: ci workflow

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Login from "@/app/auth/auth1/login/page";
 
 describe("Home", () => {
-  it('renders the component', () => {
+  it("renders the component", () => {
     render(<Login />);
     // expect(screen.getByText('Your Admin Dashboard')).toBeInTheDocument(); // Replace 'Your Text Here' with actual text or elements to check
   });
