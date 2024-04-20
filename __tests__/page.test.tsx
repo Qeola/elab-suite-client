14285a48b6afbd0c241cf0252f942bf91bb39df9
Ola Okegbemi <okegbemiolatunde@gmail.com>
2024-04-20 11:03:36 +0100

    config

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -6,6 +6,7 @@ import Login from "@/app/auth/auth1/login/page";
 describe("Home", () => {
   it("renders the component", () => {
     render(<Login />);
-    // expect(screen.getByText('Your Admin Dashboard')).toBeInTheDocument(); // Replace 'Your Text Here' with actual text or elements to check
+    
+    expect(screen.getByText('Your Admin Dashboard')).toBeInTheDocument(); // Replace 'Your Text Here' with actual text or elements to check
   });
 });
--- src/app/auth/auth1/login/page.tsx
+++ src/app/auth/auth1/login/page.tsx
@@ -80,7 +80,7 @@ export default function Login() {
         >
           <Box p={4}>
             <AuthLogin
-              title="Welcome to Modernize"
+              title="Welcome to eLab Suite"
               subtext={
                 <Typography variant="subtitle1" color="textSecondary" mb={1}>
                   Your Admin Dashboard
